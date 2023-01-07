6c6aa2d5f238e517fdcc7cbe305445a3c4958cd9
Amanilogsolution <aman@ilogsolution.com>
2023-01-07 11:38:13 +0530

    push

--- controller/Dashboard/InvoiceDashboard/Invoice.js
+++ controller/Dashboard/InvoiceDashboard/Invoice.js
@@ -9,17 +9,17 @@ const Invoice_Outstanding = async (req, res) => {
         await sql.connect(sqlConfig)
         const Vendor = await sql.query(`select count(vendor) as TotalVendor from IPERISCOPE.dbo.tbl_vendor_invoice `)
         const OutstandingAmount = await sql.query(`select SUM(convert(int,invoice_amt) ) as total from IPERISCOPE.dbo.tbl_vendor_invoice  WHERE invoice_status ='true'`)
-        const OutstandingVendor = await sql.query(`select DISTINCT(vendor) from IPERISCOPE.dbo.tbl_vendor_invoice where invoice_status ='true' `)
+        const OutstandingVendor = await sql.query(`select SUM(convert(int,invoice_amt) ) as total,vendor from IPERISCOPE.dbo.tbl_vendor_invoice  WHERE invoice_status ='true' GROUP by vendor`)
 
-        for (var val of OutstandingVendor.recordset) {
-            const values = await sql.query(`select SUM(convert(int,invoice_amt) ) as total from IPERISCOPE.dbo.tbl_vendor_invoice  WHERE invoice_status ='true'and vendor='${val.vendor}'`)
-            data.push({ name: val.vendor, value: values.recordset[0].total })
-        }
+        // for (var val of OutstandingVendor.recordset) {
+        //     const values = await sql.query(`select SUM(convert(int,invoice_amt) ) as total from IPERISCOPE.dbo.tbl_vendor_invoice  WHERE invoice_status ='true'and vendor='${val.vendor}'`)
+        //     data.push({ name: val.vendor, value: values.recordset[0].total })
+        // }
 
         res.status(200).json({
             Vendor: Vendor.recordset[0],
             OutstandingAmount: OutstandingAmount.recordset[0].total,
-            OutstandingVendor: data,
+            OutstandingVendor: OutstandingVendor.recordset,
         })
     }
     catch (err) {
